bb5853610e0fb359a4166fb84e3b568cec4090e2
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-01-03 21:20:23 +0200

    add some things

---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+const names = ["Andrii", "Anna"];
+
+export default function Post() {
+  const chooseName = Math.random() > 0.5 ? names[0] : names[1];
+  return (
+    <div>
+      <p>{chooseName}</p>
+      <p>React.js is awesome</p>
+    </div>
+  );
+}
