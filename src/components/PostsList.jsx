5cf70f85a9a9962f777e9cb86d0c36776063c05a
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-01-06 15:21:30 +0200

    add form layout

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,9 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
+
+import styles from "./PostList.module.css";
 
 export default function PostsList() {
   return (
-    <ul>
-      <Post author="Andrii" body="React is awesome!!!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={styles.list}>
+        <Post author="Andrii" body="React is awesome!!!" />
+        <Post author="Anna" body="Yeah! React is awesome!!!" />
+      </ul>
+    </>
   );
 }
