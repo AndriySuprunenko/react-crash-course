071b574d7ccd7c3d138d901eb4639b6169bfdd6c
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-01-23 17:11:24 +0200

    add submit func

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 
 import NewPost from "./NewPost";
@@ -8,26 +7,12 @@ import Modal from "./Modal";
 import styles from "./PostList.module.css";
 
 export default function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   let modalContent;
 
   if (isPosting) {
     modalContent = (
       <Modal onClose={onStopPosting}>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-          onCancel={onStopPosting}
-        />
+        <NewPost onCancel={onStopPosting} />
       </Modal>
     );
   }
@@ -36,7 +21,6 @@ export default function PostsList({ isPosting, onStopPosting }) {
     <>
       {modalContent}
       <ul className={styles.list}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Andrii" body="What's up?" />
         <Post author="Anna" body="What's going?" />
         <Post author="Arturito" body="How do you do?" />
